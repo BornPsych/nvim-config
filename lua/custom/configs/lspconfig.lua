b3e0fba55f90d25080f0a45336791d689a07f489
Kyle Andrews <k2andrews@cree.com>
2024-08-30 16:57:50 +0100

    initial commit

---
+++ lua/custom/configs/lspconfig.lua
@@ -0,0 +1,23 @@
+local base = require("plugins.configs.lspconfig")
+local on_attach = base.on_attach
+local capabilities = base.capabilities
+local util = require "lspconfig/util"
+
+local lspconfig = require("lspconfig")
+
+lspconfig.gopls.setup {
+    on_attach = on_attach,
+    capabilities = capabilities,
+    cmd = {"gopls"},
+    filetypes = { "go", "gomod", "gowork", "gotmpl" },
+    root_dir = util.root_pattern("go.work", "go.mod", ".git"),
+    settings = {
+      gopls = {
+        completeUnimported = true,
+        usePlaceholders = true,
+        analyses = {
+          unusedparams = true,
+        },
+      },
+    },
+  }
